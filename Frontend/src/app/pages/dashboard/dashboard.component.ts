9c2b8a44c26e44a94a619a7667baafcf0ead54ec
Ga1156 <gauravpardeshi068@gmail.com>
2023-09-28 00:40:51 +0530

    Implemented functionality on Dashboard page

--- Frontend/src/app/pages/dashboard/dashboard.component.ts
+++ Frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  isOpen: boolean = false;
+  isModalOpen: boolean = false;
+
+  openModal() {
+    this.isOpen = true;
+    this.isModalOpen = true;
+    console.log(this.isOpen)
+  }
+  closeModal() {
+    this.isOpen = false;
+    console.log(this.isOpen)
+  }
 
 }
